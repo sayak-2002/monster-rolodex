710eb8529d58c28be6c2b75495a5d65cd4c33ebe
Sayak Pradhan <pradhansayak2002@gmail.com>
2022-03-01 18:12:42 +0530

    monsters.rolodex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  constructor() {
+    super();  // call the constructor method on Component class.
+    this.state = {
+      // Taking array of objects
+      monsters:[],
+      searchField: '',
+    };
+  }
+  // fetching the API
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')  
+        .then((response) => response.json())
+        .then((users) => this.setState(() => {
+          return { monsters: users }
+        },
+        ));
+  }
+  
+  // setState is for modify state in React.
+
+   onSearchChange = (event) => {
+    const searchField= event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+
+  render(){
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
+    return (
+        <div className='App'>
+          <h1 className='app-title'>Monsters Rolodex</h1>
+          <SearchBox onChangeHandler={onSearchChange}  />
+          <CardList monsters={ filteredMonsters }/>
+        </div>
+    );
+  }
 }
 
 export default App;
